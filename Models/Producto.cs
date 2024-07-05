9492432ee9d1d4bf1c8cc83f0b4dda0a61adec38
SolveNaN <dagoberto6993@gmail.com>
2024-07-04 23:38:25 -0500

    Gracias por seguir este tutorial

---
+++ Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace InventarioWasm.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public double? Valor { get; set; }
+
+    }
+}
